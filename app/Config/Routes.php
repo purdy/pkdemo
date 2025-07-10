20594e941f5d614b98957e10d9fb77834e5c0398
Jason Purdy <jason@journalistic.com>
2025-07-09 23:45:31 -0400

    added check-email route/runmode

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('/check-email', 'Account::checkEmail');
 $routes->get('/account', 'Account::index');
 
--- app/Controllers/Account.php
+++ app/Controllers/Account.php
@@ -3,6 +3,24 @@
 namespace App\Controllers;
 
 class Account extends BaseController {
+  public function checkEmail() {
+    $email = $this->request->getJsonVar('email');
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $user_model = model('User');
+      $user = $user_model->where('email', $email)->first();
+      if ($user) {
+        $response = ['status' => 'exists', 'message' => 'Email already exists.'];
+      }
+      else {
+        $response = ['status' => 'available', 'message' => 'Email is available.'];
+      }
+    }
+    else {
+      $response = ['status' => 'error', 'message' => 'Invalid email address.'];
+    }
+    return $this->response->setJSON($response);
+  }
+
   public function index(): string {
     return view('account');
   }
